60079280b0c930202ddf4d9b4aede2d82718b05f
hugosakata <hugo_3wl@yahoo.com.br>
2017-10-20 11:00:18 -0200

    metodo que transforma json em objeto mes

--- memorex/app/src/main/java/memorex/progrid/base/ManipuladorJson.java
+++ memorex/app/src/main/java/memorex/progrid/base/ManipuladorJson.java
@@ -12,7 +12,45 @@ public class ManipuladorJson {
     public static Mes jsonMesToBase(String jsonText) throws JSONException{
         Mes mes = null;
 
+        JSONObject json_load = new JSONObject(jsonText);
 
+        if(json_load != null) {
+            JSONObject json_mes = json_load.getJSONObject("mes");
+
+            if (json_mes != null) {
+
+                mes = new Mes();
+                mes.setMes(json_mes.getInt("mes"));
+                mes.setAno(json_mes.getInt("ano"));
+
+                JSONArray json_dias = json_mes.getJSONArray("dias");
+
+                for (int i = 0; i < json_dias.length(); i++) {
+                    JSONObject json_dia = (JSONObject) json_dias.get(i);
+
+                    Dia dia = new Dia();
+                    dia.setDia(json_dia.getInt("dia"));
+
+                    mes.getDias().add(dia);
+
+                    JSONArray json_titulos = json_dia.getJSONArray("titulos");
+
+                    for (int j = 0; j < json_titulos.length(); j++) {
+                        JSONObject json_titulo = (JSONObject) json_dias.get(j);
+
+                        Item item = new Item();
+                        item.setId(json_titulo.getInt("id"));
+                        item.setSinal(json_titulo.getInt("sinal"));
+                        item.setGrupo(json_titulo.getInt("grupo"));
+                        item.setNome(json_titulo.getString("nome"));
+                        item.setValor(json_titulo.getString("valor"));
+
+                        dia.getItens().add(item);
+
+                    }
+                }
+            }
+        }
 
         return mes;
     }

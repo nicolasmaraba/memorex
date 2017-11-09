9f50a3b90285e07a0db8f08adace60f436ff46d4
KildareSilva <kildarealexandre@hotmail.com>
2017-11-09 12:53:43 -0200

    Inclusão do calendário na criação do lembrete.

--- memorex/app/src/main/java/memorex/progrid/activities/LembreteActivity.java
+++ memorex/app/src/main/java/memorex/progrid/activities/LembreteActivity.java
@@ -1,5 +1,7 @@
 package memorex.progrid.activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.graphics.MaskFilter;
 import android.os.Bundle;
 import android.text.style.MaskFilterSpan;
@@ -9,6 +11,7 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -21,6 +24,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.zip.DataFormatException;
 
@@ -35,13 +39,15 @@ public class LembreteActivity extends ActivityBase {
     private EditText editTextLembrete;
     private EditText editTextData;
     private int iFrequencia, iDia;
-    private Date dtEventual;
+    private SimpleDateFormat formatoData;
+    int year_x,month_x,day_x;
+    static final int DIALOG_ID = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Não sei o que é esse Linear
+     // Não sei o que é esse Linear
         LinearLayout linear = (LinearLayout) View.inflate(this,
                 R.layout.activity_lembrete, null);
         base.addView(linear, new ViewGroup.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT,
@@ -55,6 +61,11 @@ public class LembreteActivity extends ActivityBase {
         editTextData = (EditText) findViewById(R.id.editTextData);
         final TextView textViewData = (TextView) findViewById(R.id.textViewData);
         final Button btnSalvar = (Button) findViewById(R.id.btnSalvar);
+        final Button btnCalendario = (Button) findViewById(R.id.btnCalendario);
+        formatoData = new SimpleDateFormat("dd/MM/yyyy");
+        formatoData.setLenient(false);
+
+
 
         setItensDia();
         ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, itensDia);
@@ -69,6 +80,21 @@ public class LembreteActivity extends ActivityBase {
         textViewDia.setVisibility(View.GONE);
         textViewData.setVisibility(View.GONE);
         editTextData.setVisibility(View.GONE);
+        btnCalendario.setVisibility(View.GONE);
+        // Implementação do botão do calendário
+        final Calendar cal = Calendar.getInstance();
+        year_x = cal.get(Calendar.YEAR);
+        month_x  = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        btnCalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                showDialog(DIALOG_ID);
+            }
+        });
+
+
 
         spinnerFrequencia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -80,18 +106,21 @@ public class LembreteActivity extends ActivityBase {
                     textViewDia.setVisibility(View.VISIBLE);
                     textViewData.setVisibility(View.GONE);
                     editTextData.setVisibility(View.GONE);
+                    btnCalendario.setVisibility(View.GONE);
                 }
                 if (iFrequencia == 1) {
                     spinnerDia.setVisibility(View.GONE);
                     textViewDia.setVisibility(View.GONE);
                     textViewData.setVisibility(View.VISIBLE);
                     editTextData.setVisibility(View.VISIBLE);
+                    btnCalendario.setVisibility(View.VISIBLE);
                 }
                 if (iFrequencia == 0) {
                     spinnerDia.setVisibility(View.GONE);
                     textViewDia.setVisibility(View.GONE);
                     textViewData.setVisibility(View.GONE);
                     editTextData.setVisibility(View.GONE);
+                    btnCalendario.setVisibility(View.GONE);
                 }
 
             }
@@ -135,34 +164,12 @@ public class LembreteActivity extends ActivityBase {
 
     public void salvarDados() {
         String sFrequencia = "";
-        String sMensagemErro = "";
         Boolean bDataValida = false;
 
         if (iFrequencia == 1) {
             sFrequencia = "Eventual";
-            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-            formato.setLenient(false);
-            String dataEvento = editTextData.getText().toString();
-            try {
-                Date date = formato.parse(dataEvento);
-                Date dataAtual =new Date();
-                if (date.getTime() <= dataAtual.getTime()){
-                    sMensagemErro = "A data deverá ser maior que a data atual";
-                }else {
-                    if ((date.getTime() - dataAtual.getTime()) / (1000 * 60 * 60 * 24) >= 365 ) {
-                        sMensagemErro = "A data do evento não pode ser posterior a um ano";
-                    }else{
-                        bDataValida = true;
-                    }
-
-                }
-            } catch (ParseException e) {
-                sMensagemErro = "Data inválida";
-            }
-            if (bDataValida) {
+            if (validaData(editTextData.getText().toString())) {
                 Toast.makeText(getApplicationContext(), editTextLembrete.getText().toString() + " " + sFrequencia + " dia " + editTextData.getText().toString(), Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(getApplicationContext(), sMensagemErro, Toast.LENGTH_LONG).show();
             }
 
         }
@@ -172,12 +179,64 @@ public class LembreteActivity extends ActivityBase {
         }
     }
 
+
     public void setItensDia() {
         ArrayList<Integer> lista = new ArrayList<>();
         for (int i = 0; i <= 27; i++) {
             lista.add(i, i + 1);
         }
         itensDia = lista;
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID)
+            return new DatePickerDialog(this, dpickerListner, year_x, month_x, day_x);
+        return null;
+    }
+    private DatePickerDialog.OnDateSetListener dpickerListner
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear +1;
+            day_x = dayOfMonth;
+            String sMes= "" + month_x;
+            String sDia ="" + day_x;
+            if(month_x<10){
+                sMes = "0"+month_x;
+            }
+            if (day_x< 10){
+                sDia = "0"+day_x;
+            }
+            editTextData.setText( sDia+ "/" + sMes + "/" + year_x);
+        }
+    };
+
+    private boolean validaData(String sData){
+        String sMensagemErro = "";
+        boolean  bResultado = false;
+        try {
+            Date date = formatoData.parse(sData);
+            Date dataAtual =new Date();
+
+            if (date.getTime() <= dataAtual.getTime()){
+                sMensagemErro = "A data deverá ser maior que a data atual";
+            }else {
+                if ((date.getTime() - dataAtual.getTime()) / (1000 * 60 * 60 * 24) >= 365 ) {
+                    sMensagemErro = "A data do evento não pode ser posterior a um ano";
+                }else{
+                    bResultado= true;
+                }
+
+            }
+        } catch (ParseException e) {
+            sMensagemErro = "Data inválida";
+        }
+        if (sMensagemErro !=""){
+            Toast.makeText(getApplicationContext(), sMensagemErro, Toast.LENGTH_LONG).show();
+        }
+
+        return bResultado;
     }
 
 }
